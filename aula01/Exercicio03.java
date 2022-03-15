b1e8aa33ee9e313da022ae2f72c4c91830230e3b
Danilo Trindade <danilo.trindade@hotmail.com>
2022-03-15 15:58:35 -0300

    commit exercicio 3

---
+++ aula01/Exercicio03.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/*
+Escreva um programa que leia um número inteiro digitado pelo usuário e exiba uma sequência 
+de números de 1 até o valor digitado pelo usuário. 
+Por exemplo, se o usuário digitar o número 8, o programa deve mostrar
+1 2 3 4 5 6 7 8
+*/
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+
+        int mostrar;
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Informe o número");
+		mostrar = teclado.nextInt();
+		for (int contador=1; contador<=mostrar; contador ++) {
+               System.out.print(contador+" ");
+        }
+    }
+}
